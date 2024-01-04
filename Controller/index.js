56de33ad1d8aa9fb268c7b7e509270cc425d710a
yg <143371945+Yogesh699@users.noreply.github.com>
2024-01-04 22:45:46 +0500

    Handlebars has been connected well

--- Controller/index.js
+++ Controller/index.js
@@ -4,7 +4,16 @@ import { User, Post, Comment } from '../model/index.js';
 
 const router = Router();
 
-const homeRoutes = router.get('/', (req, res) => {
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+
+router.get('/', (req, res) => {
     Post.findAll({
       attributes: [
         'id',
@@ -12,20 +21,20 @@ const homeRoutes = router.get('/', (req, res) => {
         'title',
         'created_at'
       ],
-    //   include: [
-    //     {
-    //       model: Comment,
-    //       attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-    //       include: {
-    //         model: User,
-    //         attributes: ['username']
-    //       }
-    //     },
-    //     {
-    //       model: User,
-    //       attributes: ['username']
-    //     }
-    //   ]
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
     })
       .then(dbPostData => {
         const posts = dbPostData.map(post => post.get({ plain: true }));
@@ -42,7 +51,54 @@ const homeRoutes = router.get('/', (req, res) => {
       });
   });
 
-router.use('/', homeRoutes);
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'post_text',
+        'title',
+        'created_at',
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        // serialize the data
+        const post = dbPostData.get({ plain: true })
+  
+        // pass data to template
+        res.render('single-post', {
+          post,
+          loggedIn: req.session.loggedIn,
+          username: req.session.username
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+router.use('/', router);
+// router.use('/login', login);
 // router.use('/api', apiRoutes);
 // router.use('/dashboard', dashboardRoutes);
 
--- model/index.js
+++ model/index.js
@@ -2,4 +2,34 @@ import Comment from "./Comment.js";
 import User from "./User.js";
 import Post from "./Post.js";
 
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+  
+  
+  Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+  
+  Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: 'SET NULL'
+  });
+  
+  User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+  
+  Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+  });
+  
+
 export { Comment, User, Post }
